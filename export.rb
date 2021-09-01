88aa85b2a8266efb112428ad64d38e565a6fb84e
Eric Sullivan <eric.sullivan@annkissam.com>
2021-09-01 11:15:13 -0400

    export

---
+++ export.rb
@@ -0,0 +1,54 @@
+require 'octokit'
+require 'open3'
+require 'cliver'
+require 'dotenv'
+
+if ARGV.count != 1
+  puts "Usage: script/count [ORG NAME]"
+  exit 1
+end
+
+Dotenv.load
+
+def cloc(*args)
+  cloc_path = Cliver.detect! 'cloc'
+  Open3.capture2e(cloc_path, *args)
+end
+
+tmp_dir = File.expand_path "./tmp", File.dirname(__FILE__)
+FileUtils.rm_rf tmp_dir
+FileUtils.mkdir_p tmp_dir
+
+# Enabling support for GitHub Enterprise
+unless ENV["GITHUB_ENTERPRISE_URL"].nil?
+  Octokit.configure do |c|
+    c.api_endpoint = ENV["GITHUB_ENTERPRISE_URL"]
+  end
+end
+
+client = Octokit::Client.new access_token: ENV["GITHUB_TOKEN"]
+client.auto_paginate = true
+
+repos = client.organization_repositories(ARGV[0].strip, type: 'sources').reject(&:archived)
+repo_count = repos.count
+puts "Found #{repos.count} repos. Counting..."
+
+reports = []
+repos.each_with_index do |repo, index|
+  puts "(#{index} / #{repo_count}) - Exporting #{repo.name}..."
+
+  destination = File.expand_path repo.name, tmp_dir
+  report_file = File.expand_path "#{repo.name}.txt", tmp_dir
+
+  clone_url = repo.clone_url
+  clone_url = clone_url.sub "//", "//#{ENV["GITHUB_TOKEN"]}:x-oauth-basic@" if ENV["GITHUB_TOKEN"]
+  output, status = Open3.capture2e "git", "clone", "--depth", "1", "--quiet", clone_url, destination
+  next unless status.exitstatus == 0
+
+  # Remove the git info
+  FileUtils.rm_rf "#{destination}/.git"
+
+  `tar -zcvf exports/#{repo.name}.tar.gz -C #{tmp_dir} #{repo.name}`
+end
+
+puts "Done..."
